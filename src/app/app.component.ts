15e33abac3d9d1ef67dc15db42f488ad20de7e03
Angamuthu, Suresh <SAngamuthu@ashleyfurniture.com>
2021-07-15 13:30:35 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Calculator App';
+  num1: number = 0;
+  num2: number = 0;
+  result: Number = 0;
+
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  subtract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 / this.num2;
+  }
+}
